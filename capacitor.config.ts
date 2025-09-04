7f5e5d4a80f7fc4dcd0c635dacf16df7b4de9362
Ageu Assunção Simões Galindo <75844733+Bulovask@users.noreply.github.com>
2025-09-03 23:34:27 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PROJETO_REVISAO_DE_MATEMATICA_BASICA',
+  webDir: 'www'
+};
+
+export default config;
