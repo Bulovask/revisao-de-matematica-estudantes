00ee1c0734090db946f2811769de15bc1032f7d1
Ageu Assunção Simões Galindo <75844733+Bulovask@users.noreply.github.com>
2025-09-04 01:01:02 -0300

    feat(functions): inicializar funções do Firebase com configuração TypeScript
    - Adicionado package.json com scripts para linting, build, serving e deploy de funções.
    - Configuração do TypeScript com tsconfig.json e tsconfig.dev.json para desenvolvimento.
    - Criação do index.ts para configurar opções globais e preparar para exportações de funções.
    - Inclusão das dependências e devDependencies necessárias para desenvolvimento com Firebase e TypeScript.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA2dyg_-v5qkmmhRbU--x1aIRZcPjmu0uQ",
+    authDomain: "proj-revisao-de-matematica.firebaseapp.com",
+    projectId: "proj-revisao-de-matematica",
+    storageBucket: "proj-revisao-de-matematica.firebasestorage.app",
+    messagingSenderId: "554326706277",
+    appId: "1:554326706277:web:6d487d9b2498634636de8c",
+    measurementId: "G-GPPM0B5RGQ"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA2dyg_-v5qkmmhRbU--x1aIRZcPjmu0uQ",
+    authDomain: "proj-revisao-de-matematica.firebaseapp.com",
+    projectId: "proj-revisao-de-matematica",
+    storageBucket: "proj-revisao-de-matematica.firebasestorage.app",
+    messagingSenderId: "554326706277",
+    appId: "1:554326706277:web:6d487d9b2498634636de8c",
+    measurementId: "G-GPPM0B5RGQ"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
